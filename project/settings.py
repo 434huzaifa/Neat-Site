1a65612aec057cc8e68711417dbf9b0cd93c240e
Md. Huzaifa <65617127+434huzaifa@users.noreply.github.com>
2022-06-08 17:16:35 +0600

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alexjohn7372@gmail.com'
-EMAIL_HOST_PASSWORD = 'radium434'
+EMAIL_HOST_USER = 'Your Gmail'
+EMAIL_HOST_PASSWORD = 'Gmail Password'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
